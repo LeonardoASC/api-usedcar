3dbd6183bf3f3c5354cf19b8017dd9c77088f0f4
Leonardo <leonardoaschaves@gmail.com>
2024-10-18 13:03:01 -0300

    feat

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -12,6 +13,33 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
+         // Validação dos dados da requisição
+         $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8'],
+        ], [
+            'name.required' => 'O campo nome é obrigatório.',
+            'name.string' => 'O nome deve ser um texto válido.',
+            'name.max' => 'O nome não pode ter mais que 255 caracteres.',
+            
+            'email.required' => 'O campo email é obrigatório.',
+            'email.string' => 'O campo email deve ser um texto válido.',
+            'email.email' => 'O campo email deve ser um endereço de email válido.',
+            'email.max' => 'O campo email não pode ter mais que 255 caracteres.',
+            'email.unique' => 'Este email já está cadastrado.',
+    
+            'password.required' => 'O campo senha é obrigatório.',
+            'password.string' => 'A senha deve ser um texto válido.',
+            'password.min' => 'A senha deve ter no mínimo 8 caracteres.',
+            'password.confirmed' => 'A confirmação da senha não confere.',
+        ]);
+
+        // Verificar se a validação falhou
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422); 
+        }
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
